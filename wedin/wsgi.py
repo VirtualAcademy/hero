3bdc5ccea407b4c49ec0caa42da5115554e0dd81
Cho <cho@localhost.localdomain>
2016-03-15 04:13:00 -0400

    found error with wsgi confi file. Reason for gunicorn errors on heroku

--- wedin/wsgi.py
+++ wedin/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 from dj_static import Cling, MediaCling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wed.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wedin.settings")
 
 application = Cling(MediaCling(get_wsgi_application()))
